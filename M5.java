0ed0b94f66de4fd3098eba0c51271382c92079a9
Grithisgaa A S <grithisgaa200806@gmail.com>
2026-09-12 11:39:33 +0530

    added

---
+++ M5.java
@@ -0,0 +1,31 @@
+public class M5 {
+
+    static class Employee {
+
+        String empName;
+        double salary;
+
+        static String companyName = "Bright Horizon Technologies";
+        static int employeeCount = 0;
+
+        Employee(String empName, double salary) {
+            this.empName = empName;
+            this.salary = salary;
+            employeeCount++;
+        }
+
+        static void printCompanyInfo() {
+            System.out.println(companyName);
+            System.out.println("Employees on record: " + employeeCount);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Aisha", 50000);
+        Employee e2 = new Employee("Rahul", 45000);
+        Employee e3 = new Employee("Meena", 55000);
+
+        Employee.printCompanyInfo();
+    }
+}

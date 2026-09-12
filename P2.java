06c9363da69467aad5bfbb7c67ab4713845a874c
Grithisgaa A S <grithisgaa200806@gmail.com>
2026-09-12 11:42:43 +0530

    added

---
+++ P2.java
@@ -0,0 +1,49 @@
+public class P2 {
+
+    static class MessWallet {
+        private double balance;
+
+        public MessWallet(double balance) {
+            if (balance < 0) {
+                System.out.println("Warning: Negative balance not allowed");
+                this.balance = 0;
+            } else {
+                this.balance = balance;
+            }
+        }
+
+        public void topUp(double amount) {
+            if (amount <= 0) {
+                System.out.println("Invalid top-up amount");
+            } else {
+                balance += amount;
+                System.out.println("Balance after top-up: " + balance);
+            }
+        }
+
+        public void deduct(double amount) {
+            if (amount <= 0) {
+                System.out.println("Invalid deduction amount");
+            } else if (amount > balance) {
+                System.out.println("Deduct rejected: insufficient balance");
+            } else {
+                balance -= amount;
+                System.out.println("Balance after deduction: " + balance);
+            }
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MessWallet w = new MessWallet(500);
+
+        w.topUp(200);
+        w.deduct(1000);
+
+        System.out.println("Final balance: " + w.getBalance());
+    }
+}
